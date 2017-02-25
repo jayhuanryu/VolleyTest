e6c14025225567b8893ef601fcba551e41b0c676
Jay Ryu <JayRyu@CSEs-MacBook-Pro.local>
2017-02-25 14:15:45 -0800

    initial push

---
+++ app/src/main/java/edu/csusb/myapplication/MainActivity.java
@@ -0,0 +1,68 @@
+package edu.csusb.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView)findViewById(R.id.textview);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, "https://www.google.com", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        requestQueue.add(stringRequest);
+
+
+        RequestQueue postReqeustQueue = Volley.newRequestQueue(this);
+        StringRequest postStringRequest = new StringRequest(Request.Method.POST, "http://www.google.com", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", "Jay");
+                params.put("password", "1234");
+
+                return params;
+            }
+        };
+        
+        postReqeustQueue.add(postStringRequest);
+    }
+
+}
